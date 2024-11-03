ee0e696be36f0aeaea0f1596b5a4e30c884533f7
Nikita Kornilov <krnl@feip.co>
2024-11-03 06:31:21 +0600

    Add font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
     return (
         <div className="">
-            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+            <h1 className="text-3xl font-medium underline">Hello world!</h1>
+            <p className="font-regular">Hello world!</p>
         </div>
     );
 }
